27d9785c4a04b85c94276bd6891810676410d068
Loris Rimaz <loris.rimaz@gmail.com>
2023-04-05 08:47:43 +0200

    Adapté en Kaboom3000

--- breakout-mediation/principal_mediation.js
+++ breakout-mediation/principal_mediation.js
@@ -13,30 +13,20 @@ loadSound("reussite", "audio/reussite.wav");
 loadSound("echec", "audio/echec.wav");
 
 scene("accueil", () => {
-   const musique = play("musique");
+   const musique = play("musique", {
+      volume: 0.8,
+      loop: true
+   });
    add([
       text("Appuyez sur la barre d'espace pour jouer !", {
          width: 800,
       }),
-      origin("center"),
+      anchor("center"),
       pos(center()),
    ]);
-   loop(0.5, () => {
-      add([
-         text(choose(["NUMERIK", "GAMES", "SLI", "Lettres", "GAMELAB"]), {
-            width: 800,
-            font: "sink",
-            size: 48,
-         }),
-         color(randi(0, 255), randi(0, 255), randi(0, 255)),
-         origin("center"),
-         pos(randi(0, width()), randi(height() - 10, height() - 200)),
-      ]);
-   });
-
    onKeyPress("space", () => {
       go("jeu");
-      musique.stop();
+      musique.paused = true
    });
 });
 
@@ -55,33 +45,35 @@ scene("jeu", () => {
          "=xxx=x==x=xxx=",
       ],
       {
-         width: 65,
-         height: 33,
+         tileWidth: 65,
+         tileHeight: 33,
          pos: vec2(100, 200),
-         "=": () => [
-            sprite("tuile"),
-            color(255, 0, 0),
-            outline(4, 10),
-            area(),
-            solid(),
-            "brique",
-         ],
-         x: () => [
-            sprite("tuile"),
-            color(255, 0, 255),
-            outline(4, 10),
-            area(),
-            solid(),
-            "brique",
-            "special",
-         ],
+         tiles: {
+            "=": () => [
+               sprite("tuile"),
+               color(255, 0, 0),
+               outline(4, 10),
+               area(),
+               body({ isSolid: true }),
+               "brique",
+            ],
+            x: () => [
+               sprite("tuile"),
+               color(255, 0, 255),
+               outline(4, 10),
+               area(),
+               body({ isSolid: true }),
+               "brique",
+               "special",
+            ],
+         }
       }
    );
    const palet = add([
       pos(vec2(width() / 2 - 40, height() - 40)),
       rect(120, 20),
       outline(4),
-      origin("center"),
+      anchor("center"),
       area(),
       "paddle",
    ]);
@@ -92,7 +84,7 @@ scene("jeu", () => {
          size: 48,
       }),
       pos(100, 100),
-      origin("center"),
+      anchor("center"),
       z(50),
       {
          update() {
@@ -115,7 +107,7 @@ scene("jeu", () => {
          offset: vec2(-16),
       }),
       {
-         velocite: dir(rand(-60, -40)),
+         velocite: Vec2.fromAngle(rand(-60, -40)),
       },
    ]);
 
@@ -133,24 +125,24 @@ scene("jeu", () => {
          ball.pos.x = width() / 2;
          ball.pos.y = height() - 55;
          vitesse = 320;
-         ball.velocite = dir(rand(220, 290));
+         ball.velocite = Vec2.fromAngle(rand(220, 290));
          vies--;
          if (vies == 0) {
-            go("ohno", { score: score });
+            go("gameover", { score: score });
          }
       }
    });
 
    ball.onCollide("paddle", (p) => {
       vitesse += 60;
-      ball.velocite = dir(ball.pos.angle(p.pos));
+      ball.velocite = Vec2.fromAngle(ball.pos.angle(p.pos));
    });
 
    ball.onCollide("brique", (b) => {
       play("reussite");
       b.destroy();
       score++;
-      ball.velocite = dir(ball.pos.angle(b.pos));
+      ball.velocite = Vec2.fromAngle(ball.pos.angle(b.pos));
    });
    ball.onCollide("special", (b) => {
       play("reussite");
@@ -158,16 +150,16 @@ scene("jeu", () => {
       palet.color = hsl2rgb((time() * 0.2 + 1 * 0.1) % 1, 0.7, 0.8);
       palet.width = randi(50, 200);
       palet.height = randi(20, 100);
-      ball.velocite = dir(ball.pos.angle(b.pos));
+      ball.velocite = Vec2.fromAngle(ball.pos.angle(b.pos));
    });
 });
 
-scene("ohno", ({ score }) => {
+scene("gameover", ({ score }) => {
    add([
       text(`Vous avez perdu... \net fait ${score} points !`, {
          width: width(),
       }),
-      origin("center"),
+      anchor("center"),
       pos(center()),
    ]);
    onKeyPress("space", () => {
